da9ccd8e367abc8e499457117ad657b9467bc5eb
Marcus Pettersen Irgens <marcus.pettersen.irgens@gmail.com>
2021-10-05 10:44:23 +0200

    docs

--- query_test.go
+++ query_test.go
@@ -28,12 +28,16 @@ func TestQueries(t *testing.T) {
 	}
 }
 
+// testQueries is TestQueries wth different drivers.
 func testQueries(t *testing.T, driver string) {
+	// Bootstrapping
 	t.Logf("connecting to %s", *dbUrl)
 	db, err := sql.Open(driver, *dbUrl)
 	if err != nil {
 		t.Fatalf("connecting to database: %v", err)
 	}
+
+	// create a context with a deadline if the test has a deadline.
 	ctx := context.Background()
 	if ddl, hasDeadline := t.Deadline(); hasDeadline {
 		// sub a second to give the pinger some time to cancel
@@ -41,10 +45,13 @@ func testQueries(t *testing.T, driver string) {
 		ctx = c
 		defer ccl()
 	}
+
+	// wait for the database to wake up.
 	if err := pgutil.Wait(ctx, db); err != nil {
 		t.Fatalf("connecting to database: %v", err)
 	}
 
+	// Actual testing starts here.
 	q := querytest.New(db)
 
 	t.Run("Insert various Foos", func(t *testing.T) {
@@ -59,6 +66,7 @@ func testQueries(t *testing.T, driver string) {
 			time.Hour * 15,
 		}
 		for _, d := range durs {
+			// Create new rows in the Foo table using sqlc.
 			err := q.Insert(ctx, querytest.InsertParams{
 				Bar:      true,
 				Interval: int64(d),
@@ -70,6 +78,7 @@ func testQueries(t *testing.T, driver string) {
 	})
 
 	t.Run("List all Foos", func(t *testing.T) {
+		// List all the rows in the Foo table using sqlc.
 		fs, err := q.Get(ctx, math.MaxInt32)
 		if err != nil {
 			t.Errorf("fetching foos: %v", err)
